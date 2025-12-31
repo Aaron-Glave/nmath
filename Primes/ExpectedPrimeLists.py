5ac2aafa115671a5428353945c838024ea73281b
Aaron Glave <aaronglavenowbelivesingod@gmail.com>
2025-12-30 18:02:56 -0600

    Fix the test_in_order test in ExpectedPrimeLists.py

--- Primes/ExpectedPrimeLists.py
+++ Primes/ExpectedPrimeLists.py
@@ -59,7 +59,7 @@ class TestCorrectPrimes(unittest.TestCase):
         last_n = 0
         #We have 25 lines in memory already, so after running through them line_in_list should get to 1.
         line_in_list = -1*len(main.ALL_PRIMES_UNDER_100)
-        for nprime, prime in correct_prime_guess(upto=get_last_prime(), list_all=True):
+        for nprime, prime in correct_prime_guess(upto=get_last_prime()[1], list_all=True):
             line_in_list += 1
             self.assertEqual(nprime, last_n+1, msg=f"Error on line {line_in_list}")
             self.assertGreater(prime, last_p, msg=f"Error: prime located at {nprime} was less than the prime located at {last_n}")
