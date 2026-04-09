ad666c462728d4a50ef193b0ad0665757e55779c
Aaron Glave <aaronglavenowbelivesingod@gmail.com>
2026-04-09 10:01:45 -0500

    Add calculator.py
    A very simple calculator app.

---
+++ calculator.py
@@ -0,0 +1,19 @@
+"""A very simple calculator"""
+
+
+valid_chars = set(" (0123456789)+-**/")
+while True:
+    print("Expressions are numbers or"
+    " operators: + - * / ^",
+    "Parentheses () are OK.",
+    sep="\n")
+    expression = input("Expression: ").replace("^", "**")
+    if set(expression) <= valid_chars:
+        try:
+            #pylint:disable=W0123
+            print("Result:", eval(expression))
+            #pylint:enable=W0123
+        except SyntaxError:
+            print("Invalid expression")
+    else:
+        print("Invalid characters.")
