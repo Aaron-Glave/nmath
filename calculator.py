ad91a6deaa9df04efe9217b370d5a03970e9f6c7
Aaron Glave <aaronglavenowbelivesingod@gmail.com>
2026-08-24 21:54:39 -0500

    TODO: THIS DOESN'T WORK; WORK ON IT TOMORROW

--- calculator.py
+++ calculator.py
@@ -1,11 +1,36 @@
 """A very simple calculator"""
+from numbers import Number
+
+def eval_expression(exp: str):
+    safe_chars = (set('0123456789()^+-*/\\')
+        .issubset(exp))
+    error_msg_chars = ("Error: Unexpected char.\n"
+    "Expressions are numbers or"
+    " operators: . + - * / ^\n"
+    "Parentheses () are OK.")
+    assert safe_chars, error_msg_chars
+    exp = exp.replace('\\', '/')
+    output = eval(exp)
+    assert isinstance(output, Number)
+    return output
 
 
+
+def safe_eval(exp, retry_interactive=False):
+    if not retry_interactive:
+        return eval_expression(exp)
+    while True:
+        try:
+            return eval_expression(exp)
+        except AssertionError as ae:
+            print(ae)
+
+
+if __name__ == '__main__':
+    safe_eval('x=2')
 valid_chars = set(" (0.123456789)+-**/")
-while True:
-    print("Expressions are numbers or"
-    " operators: . + - * / ^",
-    "Parentheses () are OK.",
+"""while True:
+    print("
     sep="\n")
     expression = input("Expression: ").replace("^", "**")
     if set(expression) <= valid_chars:
@@ -16,4 +41,5 @@ while True:
         except SyntaxError:
             print("Invalid expression")
     else:
-        print("Invalid characters.")
+        print("Invalid characters.")"""
+
