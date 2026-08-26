6930e5b295fffadfef05b8b29b5f163d813e8d32
Aaron Glave <aaronglavenowbelivesingod@gmail.com>
2026-08-26 18:24:00 -0500

    Minor improvement to my error messages

--- Primes/AllPrimesInOrder.py
+++ Primes/AllPrimesInOrder.py
@@ -16,12 +16,18 @@ class AllPrimesInOrder(unittest.TestCase):
         #target_last_prime = get_last_prime()
         for nprime, prime in correct_prime_guess(list_all=True):
             line_in_list += 1
-            self.assertEqual(nprime, last_n + 1, msg=f"Error on line {line_in_list}")
+
+            self.assertEqual(nprime, last_n + 1,
+                             msg=(
+                                 f"Error on line {line_in_list}. "
+                                 "Expected {last_n + 1}, but got {nprime}."
+                             ))
             self.assertGreater(prime, last_p,
                                msg=(
                                    f"Error: {nprime} prime located at line {line_in_list}" +
                                    f" was less than the prime located at {last_n}"
                                ))
+
             if line_in_list % 500000 == 0 and line_in_list > 0:
                 print("Tested", line_in_list, "lines so far.")
             last_n = nprime
