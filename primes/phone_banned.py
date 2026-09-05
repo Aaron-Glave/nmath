3870fde0e9b32d2d5ed0b62368964d26a51936d6
Aaron Glave <aaronglavenowbelivesingod@gmail.com>
2026-09-05 10:37:33 -0500

    Add descriptions for PhoneBanned.py

--- primes/phone_banned.py
+++ primes/phone_banned.py
@@ -1,3 +1,5 @@
+"""A simple exception class to prevent inappropriate phone usage."""
 class PhoneBanned(Exception):
+    """No arguments required."""
     def __init__(self):
         super().__init__("Illegal to use this function on a phone.")
